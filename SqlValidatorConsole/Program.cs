23a15291e9600e6613636a3ea1f74623667ba992
Dani <daniel_plaster@live.de>
2024-11-29 14:34:31 +0100

    initial commit

---
+++ SqlValidatorConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using SqlValidator;
+
+namespace SqlValidatorConsole;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a sql statement to test:");
+        string? command;
+        while (true)
+        {
+            if (Console.ReadLine() is not string input)
+            {
+                Console.WriteLine("Something went wrong, please try again:");
+                continue;
+            }
+
+            command = input;
+            break;
+        }
+
+        ExecutableStatementValidator validator = new(command);
+
+        string result = validator.Validate() ? string.Empty : "not ";
+
+        Console.WriteLine($"Your input was {result}a succesful input.");
+    }
+}
