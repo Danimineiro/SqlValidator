a32bf853789ea8145e284ec97ad00a211e6d133d
miyujima <mewtugamer@gmail.com>
2024-12-17 14:02:43 +0100

    Progress for AlterOptions validator. Covering stuff from beginning to the start of options and column

--- SqlValidator/DDLStatements/ALTERStatements/AlterColumnValidator.cs
+++ SqlValidator/DDLStatements/ALTERStatements/AlterColumnValidator.cs
@@ -1,4 +1,3 @@
-﻿using SqlValidator.Identifiers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,32 +7,28 @@ using System.Threading.Tasks;
 namespace SqlValidator.DDLStatements.ALTERStatements;
 public static class AlterColumnValidator
 {
-    public static bool Validate(ReadOnlySpan<char> command) {
-        ReadOnlySpan<char> remainingCommand = command;
-        ReadOnlySpan<char> remaining = [];
-        if(Helper.HasNextToken(remainingCommand, "ALTER", out remaining))
+    private static int lengthCovered = 0;
+    private const int ALTER_COLUMN_TOKEN_LENGTH = 13;
+    private const int ALTER_PARAMETER_TOKEN_LENGTH = 16;
+    public static bool Validate(ReadOnlySpan<char> command)
+    {
+
+        if (command.StartsWith("ALTER COLUMN"))
         {
-            remainingCommand = remaining;
-            if (Helper.HasNextToken(remainingCommand, "COLUMN", out remaining))
-            {
-                remainingCommand = remaining;
-            }
-            else if (Helper.HasNextToken(remainingCommand, "PARAMETER", out remaining))
-            {
-                remainingCommand = remaining;
-            } else
-            {
-                return false;
-            }
+            lengthCovered += ALTER_COLUMN_TOKEN_LENGTH;
+        }
+        else if (command.StartsWith("ALTER PARAMETER"))
+        {
+            lengthCovered += ALTER_PARAMETER_TOKEN_LENGTH;
         }
         else
         {
             return false;
         }
-        if (Helper.IsNextTokenIdentifier(remainingCommand, out remaining))
+        if (IdentifierValidator.Validate(command[lengthCovered..]))
         {
-            remainingCommand = remaining;
-            if (AlterOptionsValidator.Validate(remainingCommand))
+            lengthCovered += AlterValidator.GetNextTokenLength(command[lengthCovered..]);
+            if (AlterOptionsValidator.Validate(command[lengthCovered..]))
             {
                 return true;
             }
--- SqlValidator/DDLStatements/ALTERStatements/AlterOptionsValidator.cs
+++ SqlValidator/DDLStatements/ALTERStatements/AlterOptionsValidator.cs
@@ -8,80 +8,85 @@ using System.Threading.Tasks;
 namespace SqlValidator.DDLStatements.ALTERStatements;
 public static class AlterOptionsValidator
 {
+    private const int ADD_SET_TOKEN_LENGTH = 4;
+    private const int DROP_TOKEN_LENGTH = 5;
+    private static int lengthCovered = 0;
     private static bool allOptionsCovered = false;
-
     public static bool Validate(ReadOnlySpan<char> command)
     {
-        ReadOnlySpan<char> remaining = [];
-        ReadOnlySpan<char> remainingCommand = command;
-
-        if (!Helper.HasNextToken(remainingCommand, "OPTIONS", out remaining))
-        {
-            return false;
-        }
-
-        remainingCommand = remaining;
-        if (remainingCommand.TrimStart() is not ['(', .., ')'])
-        {
-            return false;
-        }
         while (!allOptionsCovered)
         {
-            int remainingLength = remainingCommand.Length - 1;
-            remainingCommand = remainingCommand.TrimStart()[1..remainingLength];
-            if (CheckForAddSet(remainingCommand, out remaining))
+            if (command.StartsWith("OPTIONS", StringComparison.OrdinalIgnoreCase))
             {
-                remainingCommand = remaining;
-                if (Helper.IsNextTokenIdentifier(remainingCommand, out remaining))
+                lengthCovered += AlterValidator.GetNextTokenLength(command);
+                if (ParenthesesValidator.StartsAndEndsWithParentheses(command[lengthCovered..]))
                 {
-                    remainingCommand = remaining;
-                    if (Helper.IsNextTokenNumeric(remainingCommand, out remaining) || Helper.IsNextTokenNonNumericLiteral(remainingCommand, out remaining))
+                    lengthCovered += 1;
+                    if (command[lengthCovered..].StartsWith("ADD") || command[lengthCovered..].StartsWith("SET"))
                     {
-                        remainingCommand = remaining;
-                        if (!Helper.HasNextToken(remainingCommand, ",", out remaining))
+                        lengthCovered += ADD_SET_TOKEN_LENGTH;
+                        if (IdentifierValidator.Validate(command[lengthCovered..], out _))
                         {
-                            remainingCommand = remaining;
-                            allOptionsCovered = true;
+                            lengthCovered += AlterValidator.GetNextTokenLength(command[lengthCovered..]);
+                            ReadOnlySpan<char> nextToken = AlterValidator.GetNextToken(command[lengthCovered..].TrimStart());
+                            bool tokenIsNumeric = float.TryParse(nextToken, out _);
+                            if (!tokenIsNumeric)
+                            {
+                                lengthCovered += nextToken.Length;
+                                if (!command[lengthCovered..].TrimStart().StartsWith(","))
+                                {
+                                    allOptionsCovered = true;
+                                }
+                            }
+                            else
+                            {
+                                lengthCovered += nextToken.Length;
+                                if (!command[lengthCovered..].TrimStart().StartsWith(","))
+                                {
+                                    allOptionsCovered = true;
+                                }
+                            }
+                        }
+                        // TODO: ADD/SET Option list check 
+
+                    }
+                    else if (command[lengthCovered..].StartsWith("DROP"))
+                    {
+                        lengthCovered += DROP_TOKEN_LENGTH;
+                        if (IdentifierValidator.Validate(command[lengthCovered..], out _))
+                        {
+                            lengthCovered += AlterValidator.GetNextTokenLength(command[lengthCovered..]);
+                            if (!command[lengthCovered..].TrimStart().StartsWith(","))
+                            {
+                                allOptionsCovered = true;
+                            }
                         }
                     }
-                }
-
-            }
-            else if (Helper.HasNextToken(remainingCommand, "DROP", out remaining))
-            {
-                remainingCommand = remaining;
-                if (Helper.IsNextTokenIdentifier(remainingCommand, out remaining))
-                {
-                    remainingCommand = remaining;
-                    if (!Helper.HasNextToken(remainingCommand, ",", out remaining))
+                    else
                     {
-                        remainingCommand = remaining;
-                        allOptionsCovered = true;
+                        return false;
                     }
+                }
+                else
+                {
+                    return false;
                 }
             }
             else
             {
                 return false;
             }
-
         }
         return true;
     }
-    
 
-    internal static bool CheckForAddSet(ReadOnlySpan<char> command, out ReadOnlySpan<char> remaining)
+    internal static bool ValidateDROPOption(ReadOnlySpan<char> command)
     {
-        if (Helper.HasNextToken(command, "ADD", out remaining))
-        {
-            return true;
-        }
-        if (Helper.HasNextToken(command, "SET", out remaining))
-        {
-            return true;
-        }
         return false;
     }
 
-    
+    internal static bool ValidateADDSETOption(ReadOnlySpan<char> command)
+    {
+        return false;
+    }
 }
