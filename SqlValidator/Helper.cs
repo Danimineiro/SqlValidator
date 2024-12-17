110f5f71aa32319e10e509fe1b927aef8160d5d1
Benjamin Müller <116624048+benjamin-0811@users.noreply.github.com>
2024-12-17 13:58:45 +0100

    (WIP) add CreateProcedureValidator

--- SqlValidator/Helper.cs
+++ SqlValidator/Helper.cs
@@ -1,13 +1,43 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace SqlValidator;
+﻿namespace SqlValidator;
 internal static class Helper
 {
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    internal static bool SqlStartsWith(this string command, string start)
-        => command.StartsWith(start, StringComparison.OrdinalIgnoreCase);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static bool SqlStartsWith(this ReadOnlySpan<char> command, string start)
         => command.StartsWith(start, StringComparison.OrdinalIgnoreCase);
+
+    internal static bool SqlStartsWith(this ReadOnlySpan<char> command, string start, out ReadOnlySpan<char> remaining)
+    {
+        if (command.SqlStartsWith(start))
+        {
+            remaining = command[start.Length..];
+            return true;
+        }
+        remaining = command;
+        return false;
+    }
+
+    internal static bool SqlStartsWithAny(this ReadOnlySpan<char> command, string[] starts)
+    {
+        foreach (string start in starts)
+        {
+            if (command.SqlStartsWith(start))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    internal static bool SqlStartsWithAny(this ReadOnlySpan<char> command, string[] starts, out ReadOnlySpan<char> remaining)
+    {
+        foreach (string start in starts)
+        {
+            if (command.SqlStartsWith(start))
+            {
+                remaining = command[start.Length..];
+                return true;
+            }
+        }
+        remaining = command;
+        return false;
+    }
 }
