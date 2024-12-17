1af281e650394a4135c545cb5e28207e8e8b5219
Benjamin Müller <116624048+benjamin-0811@users.noreply.github.com>
2024-12-17 10:45:20 +0100

    Create Helper.cs
    - added SqlStartsWith(this string, string): bool
    - added SqlStartsWith(this ReadOnlySpan<char>, string): bool

---
+++ SqlValidator/Helper.cs
@@ -0,0 +1,9 @@
+﻿namespace SqlValidator;
+internal static class Helper
+{
+    internal static bool SqlStartsWith(this string command, string start)
+        => command.StartsWith(start, StringComparison.OrdinalIgnoreCase);
+
+    internal static bool SqlStartsWith(this ReadOnlySpan<char> command, string start)
+        => command.StartsWith(start, StringComparison.OrdinalIgnoreCase);
+}
