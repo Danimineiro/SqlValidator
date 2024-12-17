8a5eac0f9dc97efdaebdbc14447ea6132618bd05
miyujima <mewtugamer@gmail.com>
2024-12-17 14:02:43 +0100

    Progress for AlterOptions validator. Covering stuff from beginning to the start of options and column

---
+++ SqlValidator/DDLStatements/ALTERStatements/AlterColumnValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlValidator.DDLStatements.ALTERStatements;
+public static class AlterColumnValidator
+{
+    private static int lengthCovered = 0;
+    private const int ALTER_COLUMN_token_length = 13;
+    private const int ALTER_PARAMETER_token_length = 16;
+public static bool Validate(ReadOnlySpan<char> command) {
+        
+        if(command.StartsWith("ALTER COLUMN" ) || command.StartsWith("ALTER PARAMETER"))
+        {
+
+        }
+        return false;
+    }
+}
