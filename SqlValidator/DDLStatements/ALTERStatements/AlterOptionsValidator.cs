270bf948881fbd57a68c096418b76bdf2e56a045
miyujima <mewtugamer@gmail.com>
2024-12-18 14:17:53 +0100

    alter options path complete but needs testing and refactoring

--- SqlValidator/DDLStatements/ALTERStatements/AlterOptionsValidator.cs
+++ SqlValidator/DDLStatements/ALTERStatements/AlterOptionsValidator.cs
@@ -1,3 +1,4 @@
+using SqlValidator.Identifiers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,30 +11,73 @@ public static class AlterOptionsValidator
     private const int ADD_SET_TOKEN_LENGTH = 4;
     private const int DROP_TOKEN_LENGTH = 5;
     private static int lengthCovered = 0;
+    private static bool allOptionsCovered = false;
     public static bool Validate(ReadOnlySpan<char> command)
     {
-
-        if (command.StartsWith("OPTIONS", StringComparison.OrdinalIgnoreCase))
+        while (!allOptionsCovered)
         {
-            lengthCovered += AlterValidator.GetNextTokenLength(command);
-            if (ParenthesesValidator.StartsAndEndsWithParentheses(command[lengthCovered..]))
+            if (command.StartsWith("OPTIONS", StringComparison.OrdinalIgnoreCase))
             {
-                lengthCovered += 1;
-                if (command[lengthCovered..].StartsWith("ADD") || command[lengthCovered..].StartsWith("SET"))
+                lengthCovered += AlterValidator.GetNextTokenLength(command);
+                if (ParenthesesValidator.StartsAndEndsWithParentheses(command[lengthCovered..]))
                 {
-                    lengthCovered += ADD_SET_TOKEN_LENGTH;
-                }
-                else if (command[lengthCovered..].StartsWith("DROP"))
-                {
-                    lengthCovered += DROP_TOKEN_LENGTH;
-                    if (!IdentifierValidator.Validate(command[lengthCovered..]))
+                    lengthCovered += 1;
+                    if (command[lengthCovered..].StartsWith("ADD") || command[lengthCovered..].StartsWith("SET"))
+                    {
+                        lengthCovered += ADD_SET_TOKEN_LENGTH;
+                        if (IdentifierValidator.Validate(command[lengthCovered..], out _))
+                        {
+                            lengthCovered += AlterValidator.GetNextTokenLength(command[lengthCovered..]);
+                            ReadOnlySpan<char> nextToken = AlterValidator.GetNextToken(command[lengthCovered..].TrimStart());
+                            bool tokenIsNumeric = float.TryParse(nextToken, out _);
+                            if (!tokenIsNumeric)
+                            {
+                                lengthCovered += nextToken.Length;
+                                if (!command[lengthCovered..].TrimStart().StartsWith(","))
+                                {
+                                    allOptionsCovered = true;
+                                }
+                            }
+                            else
+                            {
+                                lengthCovered += nextToken.Length;
+                                if (!command[lengthCovered..].TrimStart().StartsWith(","))
+                                {
+                                    allOptionsCovered = true;
+                                }
+                            }
+                        }
+                        // TODO: ADD/SET Option list check 
+
+                    }
+                    else if (command[lengthCovered..].StartsWith("DROP"))
+                    {
+                        lengthCovered += DROP_TOKEN_LENGTH;
+                        if (IdentifierValidator.Validate(command[lengthCovered..], out _))
+                        {
+                            lengthCovered += AlterValidator.GetNextTokenLength(command[lengthCovered..]);
+                            if (!command[lengthCovered..].TrimStart().StartsWith(","))
+                            {
+                                allOptionsCovered = true;
+                            }
+                        }
+                    }
+                    else
                     {
                         return false;
                     }
                 }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
             }
         }
-        return false;
+        return true;
     }
 
     internal static bool ValidateDROPOption(ReadOnlySpan<char> command)
