685be5bc654359ea8d690249d1b96892734be5b1
Dani <daniel_plaster@live.de>
2024-12-18 15:40:36 +0100

    implement NextToken feature

--- SqlValidator/Helper.cs
+++ SqlValidator/Helper.cs
@@ -1,8 +1,73 @@
 ﻿namespace SqlValidator;
-internal static class Helper
+public static class Helper
 {
     internal static bool SqlStartsWith(this ReadOnlySpan<char> command, string start)
         => command.StartsWith(start, StringComparison.OrdinalIgnoreCase);
+
+    public static bool HasNextToken(this ReadOnlySpan<char> input, ReadOnlySpan<char> token, out ReadOnlySpan<char> remaining)
+    {
+        if (TryGetNextToken(input, out ReadOnlySpan<char> next))
+        {
+            remaining = input[next.Length..];
+            return next.Equals(token, StringComparison.OrdinalIgnoreCase);
+        }
+
+        remaining = [];
+        return false;
+    }
+
+    public static bool TryGetNextToken(this ReadOnlySpan<char> input, out ReadOnlySpan<char> token)
+    {
+        input = input.TrimStart();
+        if (input.Length < 2) 
+        {
+            token = input;
+            return true; 
+        }
+
+        switch (input[0]) 
+        {
+            case char character when character is '\'' or '"':
+                
+                for (int i = 1; i < input.Length; i++)
+                {
+                    if (input[i] != character) continue;
+
+                    if (++i < input.Length)
+                    {
+                        if (input[i] == character) continue;
+                        if (!char.IsWhiteSpace(input[i])) 
+                        {
+                            token = [];
+                            return false; 
+                        }
+                    }
+
+                    token = input[..i];
+                    return true;
+                }
+
+                // Bad input
+                token = [];
+                return false; 
+
+            case '[':
+                token = input[..(input.IndexOf(']') + 1)];
+                return true;
+
+            default:
+                int tokenEnd = input.IndexOf(' ');
+
+                if (tokenEnd == -1) 
+                {
+                    token = input;
+                    return true; 
+                }
+
+                token = input[..tokenEnd];
+                return true;
+        }
+    }
 
     internal static bool SqlStartsWith(this ReadOnlySpan<char> command, string start, out ReadOnlySpan<char> remaining)
     {
---
+++ Tests/NextTokenTests.cs
@@ -0,0 +1,45 @@
+﻿using SqlValidator;
+
+namespace Tests;
+public class NextTokenTests
+{
+    [Fact]
+    public void NextTokenString()
+    {
+        Assert.True(Helper.TryGetNextToken("\t'Hello World'", out ReadOnlySpan<char> token));
+        Assert.Equal("'Hello World'", token);
+
+        Assert.True(Helper.TryGetNextToken("'Hello World'\t", out token));
+        Assert.Equal("'Hello World'", token);
+
+        Assert.True(Helper.TryGetNextToken("\t'Hello ''World'''", out token));
+        Assert.Equal("'Hello ''World'''", token);
+
+        Assert.False(Helper.TryGetNextToken("\t'Hello World''", out _));
+        Assert.False(Helper.TryGetNextToken("\t''Hello World'", out _));
+
+        Assert.True(Helper.TryGetNextToken("Set Namespace 'Hello My Name' As \"George\"", out token));
+        Assert.Equal("Set", token);
+
+        Assert.True(Helper.TryGetNextToken(" Namespace 'Hello My Name' As \"George\"", out token));
+        Assert.Equal("Namespace", token);
+
+        Assert.True(Helper.TryGetNextToken(" 'Hello My Name' As \"George\"", out token));
+        Assert.Equal("'Hello My Name'", token);
+
+        Assert.True(Helper.TryGetNextToken(" As \"George\"", out token));
+        Assert.Equal("As", token);
+
+        Assert.True(Helper.TryGetNextToken(" \"George\"", out token));
+        Assert.Equal("\"George\"", token);
+    }
+
+    [Fact]
+    public void HasNextToken()
+    {
+        Assert.True("Set Namespace".AsSpan().HasNextToken("SET", out ReadOnlySpan<char> remaining));
+        Assert.Equal(" Namespace", remaining);
+
+        Assert.False("Set Namespace".AsSpan().HasNextToken("NAMESPACE", out _));
+    }
+}
