35951f4806f58a1f225c6e2d851248cdc3698e70
Dani <daniel_plaster@live.de>
2025-01-14 11:40:19 +0100

    implement DecimalNumericLiteralValidator

---
+++ SqlValidator/DirectlyExecutableStatements/QueryExpressions/DecimalNumericLiteralValidator.cs
@@ -0,0 +1,24 @@
+﻿
+namespace SqlValidator.DirectlyExecutableStatements.QueryExpressions;
+
+public static class DecimalNumericLiteralValidator
+{
+    public static bool Validate(ReadOnlySpan<char> input, out ReadOnlySpan<char> remainder)
+    {
+        remainder = input.TrimStart();
+        while (DigitValidator.Validate(remainder[0]))
+        {
+            remainder = remainder[1..];
+        }
+
+        if (remainder.Length < 2 || remainder[0] != '.')
+        {
+            remainder = input;
+            return false;
+        }
+
+        remainder = remainder[1..];
+
+        return UnsignedIntegerLiteralValidator.Validate(remainder, out remainder);
+    }
+}
