2f561d6aad0eea9d24f85424b32e0b969c2c2c28
Dani <daniel_plaster@live.de>
2025-01-14 11:28:25 +0100

    implement UnsignedIntegerLiteralValidator

---
+++ SqlValidator/DirectlyExecutableStatements/QueryExpressions/UnsignedIntegerLiteralValidator.cs
@@ -0,0 +1,22 @@
+﻿
+namespace SqlValidator.DirectlyExecutableStatements.QueryExpressions;
+
+public static class UnsignedIntegerLiteralValidator
+{
+    public static bool Validate(ReadOnlySpan<char> input, out ReadOnlySpan<char> remainder)
+    {
+        remainder = input.TrimStart();
+        if (input.Length < 1)
+        {
+            remainder = input;
+            return false;
+        }
+
+        while (DigitValidator.Validate(remainder[0]))
+        {
+            remainder = remainder[1..];
+        }
+
+        return true;
+    }
+}
