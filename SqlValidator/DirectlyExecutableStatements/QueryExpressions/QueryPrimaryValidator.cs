9b05668154fa77610b924b4b7ca57c209561d77c
Dani <daniel_plaster@live.de>
2025-01-14 13:54:39 +0100

    Create QueryValidators

---
+++ SqlValidator/DirectlyExecutableStatements/QueryExpressions/QueryPrimaryValidator.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SqlValidator.DirectlyExecutableStatements.QueryExpressions;
+
+internal class QueryPrimaryValidator
+{
+    internal static bool Validate(ReadOnlySpan<char> input, out ReadOnlySpan<char> remainder)
+    {
+        return QueryValidator.Validate(input, out remainder);
+    }
+}
