6f42386d35835e84263769878a9e717c25fe00fd
Dani <daniel_plaster@live.de>
2025-01-14 10:46:04 +0100

    implement HexitValidator

---
+++ SqlValidator/DirectlyExecutableStatements/QueryExpressions/HexitValidator.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Buffers;
+
+namespace SqlValidator.DirectlyExecutableStatements.QueryExpressions;
+
+public static class HexitValidator
+{
+    private static SearchValues<char> ValidValues { get; } = SearchValues.Create(['A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']);
+
+    public static bool IsValidChar(char v) => ValidValues.Contains(v);
+}
