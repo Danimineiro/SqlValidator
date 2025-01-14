d4cf657e1f45f8678f062c2c84a3d1b9b2159005
Benjamin Müller <116624048+benjamin-0811@users.noreply.github.com>
2025-01-14 13:59:02 +0100

    added GetNextWord method
    - takes input
    - outputs the next word in the input sql command
    - outputs the rest after the next word in the input sql command
    - quoted string (single and double) are treates as one word
    - special characters (. , - ? ! ~ * / \ .....) are treated as a complete word
      - special characters cannot be part of alphanumeric words (other than quoted strings)
      - special character words are only ever 1 character long
    - added tests to ensure GetNextWord's functionality
    - made all methods in Helper public to make them accessible for TestClasses

--- SqlValidator/Helper.cs
+++ SqlValidator/Helper.cs
@@ -1,10 +1,10 @@
 ﻿namespace SqlValidator;
-internal static class Helper
+public static class Helper
 {
-    internal static bool SqlStartsWith(this ReadOnlySpan<char> command, string start)
+    public static bool SqlStartsWith(this ReadOnlySpan<char> command, string start)
         => command.StartsWith(start, StringComparison.OrdinalIgnoreCase);
 
-    internal static bool SqlStartsWith(this ReadOnlySpan<char> command, string start, out ReadOnlySpan<char> remaining)
+    public static bool SqlStartsWith(this ReadOnlySpan<char> command, string start, out ReadOnlySpan<char> remaining)
     {
         if (command.SqlStartsWith(start))
         {
@@ -15,7 +15,7 @@ internal static class Helper
         return false;
     }
 
-    internal static bool SqlStartsWithAny(this ReadOnlySpan<char> command, string[] starts)
+    public static bool SqlStartsWithAny(this ReadOnlySpan<char> command, string[] starts)
     {
         foreach (string start in starts)
         {
@@ -27,7 +27,7 @@ internal static class Helper
         return false;
     }
 
-    internal static bool SqlStartsWithAny(this ReadOnlySpan<char> command, string[] starts, out ReadOnlySpan<char> remaining)
+    public static bool SqlStartsWithAny(this ReadOnlySpan<char> command, string[] starts, out ReadOnlySpan<char> remaining)
     {
         foreach (string start in starts)
         {
@@ -41,7 +41,7 @@ internal static class Helper
         return false;
     }
 
-    internal static int IntLog10(int x)
+    public static int IntLog10(int x)
     {
         if (x < 0)
             return -1;
@@ -51,10 +51,90 @@ internal static class Helper
         return log;
     }
 
-    internal static uint UIntLog10(uint x)
+    public static uint UIntLog10(uint x)
     {
         uint log = 0;
         for (; x > 0; x /= 10, log++) ;
         return log;
+    }
+
+    public static bool GetNextWord(ReadOnlySpan<char> input, out ReadOnlySpan<char> word, out ReadOnlySpan<char> rest)
+    {
+        ReadOnlySpan<char> temp = input.TrimStart();
+        if (temp.Length == 0)
+        {
+            word = "";
+            rest = "";
+            return false;
+        }    
+        switch(temp[0])
+        {
+            case '\'':
+                return GetNextQuote('\'', input, temp, out word, out rest);
+
+            case '"':
+                return GetNextQuote('"', input, temp, out word, out rest);
+
+            default:
+                break;
+        }
+        if (IsWordChar(temp[0]))
+        {
+            int i = 1;
+            for (; i < temp.Length; i++)
+            {
+                if (!IsWordChar(temp[i]))
+                {
+                    word = temp[..i];
+                    rest = temp[i..];
+                    return true;
+                }
+            }
+            word = temp;
+            rest = "";
+            return true;
+        }
+        // special char
+        word = temp[..1];
+        rest = temp[1..];
+        return true;
+    }
+
+    private static bool GetNextQuote(char quoteChar, ReadOnlySpan<char> input, ReadOnlySpan<char> temp, out ReadOnlySpan<char> word, out ReadOnlySpan<char> rest)
+    {
+        int i = 1;
+        for (; i < temp.Length; i++)
+        {
+            if (temp[i] == quoteChar)
+            {
+                if (i == temp.Length - 1)
+                {
+                    word = temp;
+                    rest = string.Empty;
+                    return true;
+                }
+                if (temp[i - 1] == '\\' || temp[++i] == quoteChar)
+                {
+                    continue;
+                }
+                word = temp[..i];
+                rest = temp[i..];
+                return true;
+            }
+        }
+        if (i == temp.Length)
+        {
+            word = string.Empty;
+            rest = input;
+            return false;
+        }
+        word = temp[..i];
+        rest = temp[i..];
+        return true;
+    }
+
+    private static bool IsWordChar(this char c)
+    {
+        return LetterValidator.Validate(c) || DigitValidator.Validate(c) || c == '_';
     }
 }
