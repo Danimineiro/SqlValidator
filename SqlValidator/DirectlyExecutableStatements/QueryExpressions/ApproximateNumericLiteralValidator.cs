6094132a5a403f22dc716903144d1a29a1950dc0
Dani <daniel_plaster@live.de>
2025-01-14 11:33:44 +0100

    implement ApproximateNumericLiteralValidator

---
+++ SqlValidator/DirectlyExecutableStatements/QueryExpressions/ApproximateNumericLiteralValidator.cs
@@ -0,0 +1,24 @@
+﻿
+namespace SqlValidator.DirectlyExecutableStatements.QueryExpressions;
+
+public static class ApproximateNumericLiteralValidator
+{
+    public static bool Validate(ReadOnlySpan<char> input, out ReadOnlySpan<char> remainder)
+    {
+        if (!UnsignedIntegerLiteralValidator.Validate(input, out remainder)) return false;
+
+        if (char.ToLower(remainder[0]) != 'e')
+        {
+            remainder = input;
+            return false;
+        }
+
+        //Remove plus or minus if present
+        PlusOrMinusValidator.Validate(input, out remainder);
+
+        if (UnsignedIntegerLiteralValidator.Validate(input, out remainder)) return true;
+
+        remainder = input;
+        return false;
+    }
+}
