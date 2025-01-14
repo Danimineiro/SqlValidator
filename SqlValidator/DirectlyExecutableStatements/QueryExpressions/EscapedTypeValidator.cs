ff601ca9e368d24e4c3556064c57b68fdfc8d7bb
Dani <daniel_plaster@live.de>
2025-01-14 11:12:03 +0100

    implement EscapedTypeValidator

---
+++ SqlValidator/DirectlyExecutableStatements/QueryExpressions/EscapedTypeValidator.cs
@@ -0,0 +1,37 @@
+﻿
+namespace SqlValidator.DirectlyExecutableStatements.QueryExpressions;
+
+public static class EscapedTypeValidator
+{
+    private static readonly char[] singleValidChar = ['d', 't', 'b'];
+
+    public static bool Validate(ReadOnlySpan<char> input, out ReadOnlySpan<char> remainder)
+    {
+        remainder = input.TrimStart();
+        if (remainder.Length < 2 || remainder[0] != '{')
+        {
+            remainder = input;
+            return false;
+        }
+
+        remainder = remainder[1..];
+
+        if (singleValidChar.Contains(remainder[0]))
+        {
+            if (remainder[0] != 't')
+            {
+                remainder = remainder[1..];
+                return true;
+            }
+
+            if (remainder.Length >= 2 && remainder[1] == 's')
+            {
+                remainder = remainder[2..];
+                return true;
+            }
+        }
+
+        remainder = input;
+        return false;
+    }
+}
