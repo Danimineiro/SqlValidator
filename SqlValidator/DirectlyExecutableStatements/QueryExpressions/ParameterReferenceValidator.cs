3b09535239d7e62316e070fc4e0e51e1b352a9f3
Dani <daniel_plaster@live.de>
2025-01-14 12:01:40 +0100

    implement ParameterReferenceValidator

---
+++ SqlValidator/DirectlyExecutableStatements/QueryExpressions/ParameterReferenceValidator.cs
@@ -0,0 +1,23 @@
+﻿
+namespace SqlValidator.DirectlyExecutableStatements.QueryExpressions;
+
+public static class ParameterReferenceValidator
+{
+    public static bool Validate(ReadOnlySpan<char> input, out ReadOnlySpan<char> remainder)
+    {
+        if (input[0] == '?')
+        {
+            remainder = input[1..];
+            return true;
+        }
+
+        if (input[0] == '$')
+        {
+            remainder = input[1..];
+            if (UnsignedIntegerValidator.Validate(remainder, out remainder)) return true;
+        }
+
+        remainder = input;
+        return false;
+    }
+}
